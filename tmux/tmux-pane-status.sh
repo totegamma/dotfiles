ec6b02b89a57ab52714cfe81302b41c9071e8acc
thotgamma <github@gammalab.net>
2022-07-06 10:41:11 +0900

    change: use python to parse args

--- tmux/tmux-pane-status.sh
+++ tmux/tmux-pane-status.sh
@@ -7,22 +7,26 @@ CURRENTDIR=$3
 if [[ $PROG = "ssh" ]]; then
 
 	COMMAND=$(pgrep -aP $PID | awk '{print substr($0, length($1)+2)}')
-	ARGS=(`echo $COMMAND`)
-	positional_args=()
 
-	for ELEM in $ARGS; do
-		if [[ $ELEM != -* ]]; then
-			positional_args+=$ELEM
-		fi
-	done
+	python3 - $COMMAND << EOF
+import argparse
+import sys
 
-	echo " #[fg=brightwhite]REMOTE $positional_args[2] #[fg=colour240,bg=default]"
+parser = argparse.ArgumentParser()
+parser.add_argument('cmd')
+parser.add_argument('address')
+parser.add_argument('-p', type=int)
+args = parser.parse_known_args(sys.argv[1].split())[0]
+
+print(f" #[fg=brightwhite]REMOTE {args.address} #[fg=colour240,bg=default]")
+EOF
 	exit
 fi
 
 if [[ $PROG = "docker" ]]; then
 
 	COMMAND=$(pgrep -aP $PID | awk '{print substr($0, length($1)+2)}')
+
 	ARGS=(`echo $COMMAND`)
 	positional_args=()
 
