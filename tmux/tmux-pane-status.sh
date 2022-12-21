656a86c829025652c0cf39fd909675858ca73bc2
totegamma <ryosuke.yanagisawa@gree.net>
2022-12-21 13:48:46 +0900

    change decoration triangle to slant

--- tmux/tmux-pane-status.sh
+++ tmux/tmux-pane-status.sh
@@ -6,9 +6,9 @@ CURRENTDIR=$3
 
 if [[ $PROG = "ssh" ]]; then
 
-	COMMAND=$(pgrep -aflP $PID | awk '{print substr($0, length($1)+2)}')
+    COMMAND=$(pgrep -aflP $PID | awk '{print substr($0, length($1)+2)}')
 
-	python3 - $COMMAND << EOF
+    python3 - $COMMAND << EOF
 import argparse
 import sys
 
@@ -18,28 +18,28 @@ parser.add_argument('address')
 parser.add_argument('-p', type=int)
 args = parser.parse_known_args(sys.argv[1].split())[0]
 
-print(f" #[fg=brightwhite]REMOTE {args.address} #[fg=colour240,bg=default]")
+print(f" #[fg=brightwhite]REMOTE {args.address} #[fg=colour240,bg=default]")
 EOF
-	exit
+exit
 fi
 
 if [[ $PROG = "docker" ]]; then
 
-	COMMAND=$(pgrep -aflP $PID | awk '{print substr($0, length($1)+2)}')
+    COMMAND=$(pgrep -aflP $PID | awk '{print substr($0, length($1)+2)}')
 
-	ARGS=(`echo $COMMAND`)
-	positional_args=()
+    ARGS=(`echo $COMMAND`)
+    positional_args=()
 
-	for ELEM in $ARGS; do
-		if [[ $ELEM != -* ]]; then
-			positional_args+=$ELEM
-		fi
-	done
+    for ELEM in $ARGS; do
+        if [[ $ELEM != -* ]]; then
+            positional_args+=$ELEM
+        fi
+    done
 
-	if [[ $positional_args[2] = "exec" ]]; then
-        docker ps -aqf "id=${positional_args[3]}" --format '{{" "}}#[fg=white]{{.Image}}  {{.ID}} #[fg=colour240,bg=default]'
-		exit
-	fi
+    if [[ $positional_args[2] = "exec" ]]; then
+        /usr/local/bin/docker ps -aqf "id=${positional_args[3]}" --format '{{" "}}#[fg=white]{{.Image}}   {{.ID}} #[fg=colour240,bg=default]'
+        exit
+    fi
 fi
 
 cd $CURRENTDIR && python3 ~/.tmux/powerline-client.py tmux left -p ~/.tmux/powerline
